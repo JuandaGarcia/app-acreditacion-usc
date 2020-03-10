d85be52734162b0f9d743b04dddcd62a7946e121
sirzes02 <sirzes02@gmail.com>
2020-03-09 22:41:02 -0500

    cambios menores

--- src/screens/ScanQR.js
+++ src/screens/ScanQR.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Dimensions } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import Navbar from "../components/Navbar";
 
 const ScanQR = () => {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
   useEffect(() => {
@@ -20,25 +21,77 @@ const ScanQR = () => {
     }
   };
 
-  if (hasPermission === null)
-    return <Text>Requesting for camera permission</Text>;
-  if (hasPermission === false) return <Text>No access to camera</Text>;
+  const { width } = Dimensions.get("screen");
+  const leftTop = {
+    borderLeftWidth: 8,
+    borderTopWidth: 8,
+    borderColor: "#8482FF"
+  };
+  const leftBottom = {
+    borderLeftWidth: 8,
+    borderBottomWidth: 8,
+    borderColor: "#8482FF"
+  };
+  const rightTop = {
+    borderRightWidth: 8,
+    borderTopWidth: 8,
+    borderColor: "#8482FF"
+  };
+  const rightBottom = {
+    borderRightWidth: 8,
+    borderBottomWidth: 8,
+    borderColor: "#8482FF"
+  };
+
   return (
     <View
       style={{
         flex: 1,
-        flexDirection: "column",
-        justifyContent: "flex-end"
+        flexDirection: "column"
       }}
     >
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          marginTop: 15
+        }}
       />
-
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-      )}
+      <View
+        style={{
+          ...StyleSheet.absoluteFill,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <View
+          style={{
+            width: width / 2,
+            height: width / 2
+          }}
+        >
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <View style={{ flex: 1, ...leftTop }} />
+            <View style={{ flex: 1 }} />
+            <View style={{ flex: 1, ...rightTop }} />
+          </View>
+          <View style={{ flex: 1 }} />
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <View style={{ flex: 1, ...leftBottom }} />
+            <View style={{ flex: 1 }} />
+            <View style={{ flex: 1, ...rightBottom }} />
+          </View>
+        </View>
+      </View>
+      <View style={{ marginTop: 60 }}>
+        {scanned && (
+          <Button
+            title={"Volver a escanear"}
+            onPress={() => setScanned(false)}
+          />
+        )}
+      </View>
+      <Navbar />
     </View>
   );
 };
