11abec9ae6260f63fad837d62e7fa8f01220ee7e
Juan David <gottygarcia@gmail.com>
2020-03-16 20:39:03 -0500

    Preguntas organizadas en la interfaz

--- App.js
+++ App.js
@@ -1,33 +1,28 @@
-import React from "react";
-import { NativeRouter, Switch, Route } from "react-router-native";
-import Weeks from "./src/screens/Weeks";
-import ScanQR from "./src/screens/ScanQR";
-import Ranking from "./src/screens/Ranking";
-import Layout from "./src/components/Layout";
-import PreguntasSemana from "./src/screens/PreguntasSemana";
-
-import Preguntas from "./src/models/preguntas/Preguntas";
-var preguntas = new Preguntas();
-
-alert(preguntas.buscarPorSemana(1)[13].title);
+import React from 'react'
+import { NativeRouter, Switch, Route } from 'react-router-native'
+import Weeks from './src/screens/Weeks'
+import ScanQR from './src/screens/ScanQR'
+import Ranking from './src/screens/Ranking'
+import Layout from './src/components/Layout'
+import PreguntasSemana from './src/screens/PreguntasSemana'
 
 const App = () => {
-  return (
-    <NativeRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Weeks} />
-          <Route exact path="/scan-qr" component={ScanQR} />
-          <Route exact path="/ranking" component={Ranking} />
-          <Route
-            exact
-            path="/preguntas-semana/:numero"
-            component={PreguntasSemana}
-          />
-        </Switch>
-      </Layout>
-    </NativeRouter>
-  );
-};
+	return (
+		<NativeRouter>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Weeks} />
+					<Route exact path="/scan-qr" component={ScanQR} />
+					<Route exact path="/ranking" component={Ranking} />
+					<Route
+						exact
+						path="/preguntas-semana/:numero"
+						component={PreguntasSemana}
+					/>
+				</Switch>
+			</Layout>
+		</NativeRouter>
+	)
+}
 
-export default App;
+export default App
